7c9f9d1b7b02ffbc749eb3094bcee33e8ab18df0
luan.carmo <luan.carmo@escolar.ifrn.edu.br>
2022-01-14 16:20:12 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,33 @@
+import MovieCard from "components/Moviecard";
 import Pagination from "components/Navbar/Pagination";
 
 function Listing() {
 
-    return(
-        <Pagination />
+    return (
+
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
